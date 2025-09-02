a30e8fd88b9b20607c64f7b8cfeac7ea3967b587
Kyle West <kw@enerknol.com>
2025-09-02 12:39:56 +0200

    initial commit, creating nege_max.py

---
+++ nege_max.py
@@ -0,0 +1,7 @@
+# Implementation from lecture
+#
+# 1. Heuristic values are from the perspective of the acting player
+# 2. Alpha a lower bound on the best value that the acting player can achieve
+# 3. Beta is an upper bound on what the opponent can achieve
+#
+
